bf5cb915551e9f1bde2b52d16bfef8400849bed5
ludmila davletshina <ludaplv@mail.ru>
2024-06-26 12:49:50 +0300

    созданы папки

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+# Вспомогательные функции, необходимые для работы функции страницы «Главная»
+# Функция для страницы «Главная» принимает на вход DataFrame
+# используют библиотеку json, используют API, используют библиотеку datetime, logging, pandas
+
+# Вспомогательные функции, необходимые для работы функции страницы «События»,
+# отдает корректный JSON-ответ
+# используют библиотеку json, используют API, datetime, logging, logging
+import pandas as pd
+def read_excel_file(input_file):
+    """ Считывание excel файла и преобразование в словарь"""
+    df = pd.read_excel(input_file)
+    df_dict = df.to_dict(orient="records")
+    return df_dict
+
+
+input_file = "../../../../my_pj/coursework_bank_application/data/operations Mon Jan 01 20_45_05 MSK 2024-Mon Jun 24 17_37_09 MSK 2024.xls"
+print(read_excel_file(input_file))
