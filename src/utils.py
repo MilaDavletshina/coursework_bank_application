4722357f8a6c1c537110e97787b2f6a3809dc01f
ludmila davletshina <ludaplv@mail.ru>
2024-06-26 13:47:19 +0300

    реализована функция считывания эксель файла и преобразования в словарь

--- src/utils.py
+++ src/utils.py
@@ -6,12 +6,12 @@
 # отдает корректный JSON-ответ
 # используют библиотеку json, используют API, datetime, logging, logging
 import pandas as pd
+
 def read_excel_file(input_file):
     """ Считывание excel файла и преобразование в словарь"""
     df = pd.read_excel(input_file)
     df_dict = df.to_dict(orient="records")
     return df_dict
 
-
-input_file = "../../../../my_pj/coursework_bank_application/data/operations Mon Jan 01 20_45_05 MSK 2024-Mon Jun 24 17_37_09 MSK 2024.xls"
+input_file = "data/operations Mon Jan 01 20_45_05 MSK 2024-Mon Jun 24 17_37_09 MSK 2024.xls"
 print(read_excel_file(input_file))
