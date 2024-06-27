ecaf69e1b10a968bddc06eaf8fed2f1774c3efb7
ludmila davletshina <ludaplv@mail.ru>
2024-06-27 11:15:48 +0300

    реализована функция по карте card_operations_info

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,3 @@
-# Вспомогательные функции, необходимые для работы функции страницы «Главная»
-# Функция для страницы «Главная» принимает на вход DataFrame
-# используют библиотеку json, используют API, используют библиотеку datetime, logging, pandas
-
-# Вспомогательные функции, необходимые для работы функции страницы «События»,
-# отдает корректный JSON-ответ
-# используют библиотеку json, используют API, datetime, logging, logging
 import pandas as pd
 
 def read_excel_file(input_file):
@@ -13,6 +6,36 @@ def read_excel_file(input_file):
     df_dict = df.to_dict(orient="records")
     return df_dict
 
-input_file = "data/operations Mon Jan 01 20_45_05 MSK 2024-Mon Jun 24 17_37_09 MSK 2024.xls"
-print(read_excel_file(input_file))
+# input_file = "data/operations Mon Jan 01 20_45_05 MSK 2024-Mon Jun 24 17_37_09 MSK 2024.xls"
+# print(read_excel_file(input_file))
 
+# def read_excel_file(input_file: list) -> list[dict]:
+#     """ Считывание excel файла и преобразование в словарь"""
+#     data_list = []
+#     for transaction in input_file:
+#         if pd.notnull(transaction["Номер карты"]):
+#             result = {
+#                 "Дата операции": transaction["Дата операции"],
+#                 "Дата платежа": transaction["Дата платежа"],
+#                 "Номер карты": str(transaction["Номер карты"])[-4:],
+#                 "Статус": transaction["Статус"],
+#                 "Сумма операции": float(transaction["Сумма операции"]),
+#                 "Валюта операции": transaction["Валюта операции"],
+#                 "Сумма платежа": float(transaction["Сумма платежа"]),
+#                 "Валюта платежа": transaction["Валюта платежа"],
+#                 "Кэшбэк": float(transaction["Кэшбэк"]),
+#                 "Категория": transaction["Категория"],
+#                 "MCC": transaction["MCC"],
+#                 "Описание": transaction["Описание"],
+#                 "Бонусы (включая кэшбэк)": float(transaction["Бонусы (включая кэшбэк)"]),
+#                 "Округление на инвесткопилку": float(transaction["Округление на инвесткопилку"]),
+#                 "Сумма операции с округлением": float(transaction["Сумма операции с округлением"])
+#              }
+#             data_list.append(result)
+#         else:
+#             result = {}
+#             data_list.append(result)
+#     return data_list
+#
+# input_file = "data/operations Mon Jan 01 20_45_05 MSK 2024-Mon Jun 24 17_37_09 MSK 2024.xls"
+# print(read_excel_file(input_file))
--- src/views.py
+++ src/views.py
@@ -1,4 +1,6 @@
 import datetime
+import pandas as pd
+
 
 def greeting():
     """Функция приветствия в зависимости от времени суток"""
@@ -12,3 +14,33 @@ def greeting():
     if now.hour >= 0 and now.hour <= 6 :
         return "Добрый ночи"
 print(greeting())
+
+
+def card_operations_info(transactions):
+    """Функция выводит общую информацию по карте"""
+    data = pd.read_excel(transactions)
+
+    card_data = {}
+
+    for index, row in data.iterrows():
+        card_number = str(row['Номер карты'])[-4:] if pd.notnull(row['Номер карты']) else None
+        total_spent = row['Сумма операции с округлением']
+
+        if card_number in card_data:
+            card_data[card_number]['total_spent'] += total_spent
+        else:
+            card_data[card_number] = {
+                'last_digits': card_number,
+                'total_spent': total_spent,
+                'cashback': total_spent // 100
+            }
+
+    result = [value for value in card_data.values()]
+
+    return result
+
+transactions = 'data/operations Mon Jan 01 20_45_05 MSK 2024-Mon Jun 24 17_37_09 MSK 2024.xls'
+result = card_operations_info(transactions)
+print(result)
+
+
